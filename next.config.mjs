e6f07bfdcdeb6120cb74f42c235d3898ff1ea2ba
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2023-10-13 05:09:10 +0530

    Lanfing page

--- next.config.mjs
+++ next.config.mjs
@@ -6,16 +6,10 @@ import { env } from "./env.mjs"
  * @type {import('next').NextConfig}
  */
 const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
+  output: "export",
   reactStrictMode: true,
   experimental: { instrumentationHook: true },
-  rewrites() {
-    return [
-      { source: "/healthz", destination: "/api/health" },
-      { source: "/api/healthz", destination: "/api/health" },
-      { source: "/health", destination: "/api/health" },
-      { source: "/ping", destination: "/api/health" },
-    ]
-  },
+  
 })
 
 export default config
---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Navbar from "components/navbar/Navbar"
+
+const about = () => {
+  return (
+    <div className="h-screen bg-black text-white">
+      <Navbar />
+      About us page
+    </div>
+  )
+}
+
+export default about
